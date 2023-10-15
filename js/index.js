f4ad9d045da87f38023823d2d9c8113be1978e17
AtharvaPC <baodhankaratharva@gmail.com>
2023-10-15 10:19:48 +0530

    added new web dev tutorials

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 var navbar = document.querySelector("nav");
 var hamburger = document.querySelector(".hamburger");
+var version = document.querySelector("#version");
+var t_btn = document.querySelector(".t-btn");
+var t_show = document.querySelector(".t-show");
+
+
+
+version.innerHTML = "~Version 3.2";
 
 hamburger.addEventListener("click", () => {
    navbar.classList.toggle("active");
@@ -17,4 +24,14 @@ function closeop() {
    navbar.classList.remove("active");
    hamburger.classList.remove("active");
 }
+
+t_btn.addEventListener("click", () => {
+   t_show.classList.toggle("active");
+   t_btn.classList.toggle("active");
+
+   
+})
+
+
+
  
